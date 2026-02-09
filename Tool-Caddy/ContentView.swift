afac1da4d04dfeefdef46d5b847ac759fa92a721
Sohum Mogale <sohummogale@student.unsw.edu.au>
2026-02-09 18:52:01 +1100

    Initial Xcode project setup

---
+++ Tool-Caddy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tool-Caddy
+//
+//  Created by Sohum Mogale on 9/2/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tool-Caddy/Tool_CaddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tool_CaddyApp.swift
+//  Tool-Caddy
+//
+//  Created by Sohum Mogale on 9/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Tool_CaddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
