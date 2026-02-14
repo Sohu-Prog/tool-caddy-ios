b78f0b3f67a6be10cb9d177fc3f2c68fd2b26b5c
Sohum Mogale <sohummogale@student.unsw.edu.au>
2026-02-14 20:13:55 +1100

    Updated cameraview to better accomadate preview logic

--- Tool-Caddy/CameraView.swift
+++ Tool-Caddy/CameraView.swift
@@ -11,46 +11,62 @@ import SwiftUI
 struct CameraView: View {
     @StateObject private var cameraManager = CameraManager()
     @State private var capturedImage: UIImage?
-    
+    @State private var buttonPressed: Bool = false
     
     var body: some View {
+        let isPreview = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
+        
         ZStack {
-            #if targetEnvironment(simulator)
-            Color.gray
-            #else
             
+            if(isPreview) {
+               previewPlaceholder
+            } else {
                 CameraPreview(session: cameraManager.session).ignoresSafeArea()
-            #endif
+            }
+            
+               
             
             VStack {
                 Spacer()
                 
                 Button(action: {
-                    cameraManager.capturePhoto()
+                    if !isPreview {
+                        cameraManager.capturePhoto()
+                    }
+                    buttonPressed = true
                 }) {
                     Circle().fill(Color.blue)
                         .frame(width: 70, height: 70)
                         .overlay(Circle().stroke(Color.black, lineWidth:2))
                 }
                 .padding(.bottom, 30)
+                
+                if (isPreview && buttonPressed) {
+                    Text("Feature not available in Preview").font(Font.largeTitle.bold()).foregroundColor(.blue).zIndex(3)
+                }
             }
         }
         .onAppear {
-            #if !targetEnvironment(simulator)
-            cameraManager.onPhotoCaptured = { image in
-                capturedImage = image
-                // ML code
+            if !isPreview {
+                cameraManager.onPhotoCaptured = { image in
+                    capturedImage = image
+                    // ML code
+                }
+                cameraManager.startSession()
             }
-            cameraManager.startSession()
-            #endif
         }
         .onDisappear {
-            #if !targetEnvironment(simulator)
-            cameraManager.stopSession()
-            #endif
+            if !isPreview {
+                cameraManager.stopSession()
+            }
         }
     }
     
+    var previewPlaceholder: some View {
+        Color.gray
+        .overlay(Text("Camera Preview Placeholder"))
+        .foregroundColor(Color.white)
+    }
     
     
 }
