6560d2fc421a06fae6aef1a9724e560463e32cc8
Sohum Mogale <sohummogale@student.unsw.edu.au>
2026-04-02 16:56:18 +1100

    Baked Normalisation into model using wrapper class ModelWithNorm. Exported new CoreMLPackage into swift

--- Model/export.py
+++ Model/export.py
@@ -2,23 +2,29 @@ import coremltools as ct
 import torch
 import torchvision.models as models
 import torch.nn as nn
-
+from train_pipeline import ModelWithNorm
 NUM_CLASSES = 3
 
-# pretrain=False may be deprecated in future, so use below
-# mv3_model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
 
-model = models.mobilenet_v3_large(pretrained=False)
+model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
 model.classifier[3] = nn.Linear(in_features=1280, out_features=NUM_CLASSES)
-model.load_state_dict(torch.load("toy_model_weights.pth"))
+
+model = ModelWithNorm(model)
+
+
+model.load_state_dict(torch.load("normalised_model_weights.pth"))
+
 model.eval()
 
-sample_input = torch.rand(1, 3, 244, 244)
+sample_input = torch.rand(1, 3, 224, 224)
 traced = torch.jit.trace(model, sample_input)
 
 coreml_model = ct.convert(traced, 
-                          inputs=[ct.ImageType(name="Input", shape = (1,3,244,244))]
-                          )
+                          inputs=[ct.ImageType(
+                            name="input",
+                            shape=(1, 3, 224, 224),
+                        )]
+                    )
 
 coreml_model.save("MobileNetV3Classifier.mlpackage")
 
