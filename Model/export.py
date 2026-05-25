Fixed scaling during model exporting to prevent normalisation bug

Model/export.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 from train_pipeline import ModelWithNorm
 NUM_CLASSES = 3
 
-
 model = models.mobilenet_v3_large(weights=models.MobileNet_V3_Large_Weights.DEFAULT)
 model.classifier[3] = nn.Linear(in_features=1280, out_features=NUM_CLASSES)
 
@@ -23,6 +22,7 @@ coreml_model = ct.convert(traced,
                           inputs=[ct.ImageType(
                             name="input",
                             shape=(1, 3, 224, 224),
+                            scale=1/255.0,
                         )],
                         classifier_config=ct.ClassifierConfig(
                         class_labels=["Fairway", "Rough", "HardPan"]